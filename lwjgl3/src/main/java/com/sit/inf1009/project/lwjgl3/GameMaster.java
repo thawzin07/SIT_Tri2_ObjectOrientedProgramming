2f9e8135430db9d5e942f309841d2665d62b98a8
Gerome <2503678@sit.singaporetech.edu.sg>
2026-02-16 21:46:38 +0800

    Basic GameMaster import

---
+++ lwjgl3/src/main/java/com/sit/inf1009/project/lwjgl3/GameMaster.java
@@ -0,0 +1,11 @@
+package com.sit.inf1009.project.lwjgl3;
+
+import com.sit.inf1009.project.engine.managers.*;
+
+public class GameMaster {
+	private EntityManager entityManager;
+	private CollisionManager collisionManager;
+	private InputOutputManager ioManager;
+	private MovementManager movementManager;
+	private SceneManager sceneManager;
+}
